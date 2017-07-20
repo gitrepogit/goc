e00dcf9a246f6c3548ff57ce217ace775a041916
HA <gitrepo.git@gmail.com>
2017-07-20 22:45:27 +0500

    Changed Top Ranked GM Tabs, added Random Pick to Home View, added Image URL field to Details View

--- src/app/home/home.component.js
+++ src/app/home/home.component.js
@@ -15,11 +15,30 @@ var HomeComponent = (function () {
     function HomeComponent(gocService) {
         this.gocService = gocService;
         this.gocs = [];
+        // The URL of the image which is displayed if a picture of the grandmaster is not available
+        this.placeHolder = '/assets/app-images/no-image.jpg';
     }
     HomeComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.gocService.getGocs()
-            .then(function (gocs) { return _this.gocs = gocs.slice(1, 5); });
+            .then(function (gocs) {
+            _this.gocs = (gocs.sort(function (item1, item2) {
+                if (item1.rating > item2.rating) {
+                    return -1;
+                }
+                if (item1.rating < item2.rating) {
+                    return 1;
+                }
+                return 0;
+            })).slice(0, 4); // return top rated grandmasters
+            _this.randomGoc = gocs[Math.floor(Math.random() * gocs.length)]; //get random goc
+        });
+    };
+    // display a placeholder image if the picture of the selected grandamster is not available
+    HomeComponent.prototype.noImage = function (goc) {
+        if (goc.image !== this.placeHolder) {
+            goc.image = this.placeHolder;
+        }
     };
     return HomeComponent;
 }());
@@ -34,4 +53,5 @@ HomeComponent = __decorate([
     __metadata("design:paramtypes", [goc_service_1.GocService])
 ], HomeComponent);
 exports.HomeComponent = HomeComponent;
+//gocs.slice(1, 5) 
 //# sourceMappingURL=home.component.js.map
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,11 +12,39 @@ import { GocService } from '../gocs/goc.service';
 // Home page
 export class HomeComponent implements OnInit {
   gocs: Goc[] = [];
+  randomGoc: Goc;
+  // The URL of the image which is displayed if a picture of the grandmaster is not available
+  private placeHolder: string = '/assets/app-images/no-image.jpg'; 
 
   constructor(private gocService: GocService) { }
 
   ngOnInit(): void {
     this.gocService.getGocs()
-      .then(gocs => this.gocs = gocs.slice(1, 5));
+      .then(gocs => {
+              this.gocs = (gocs.sort((item1, item2) => { //Sort the list of grandmasters, highest to lowest rank
+  													if (item1.rating > item2.rating) { return -1; }
+  													if (item1.rating < item2.rating) { return 1; }
+  													return 0;
+												  }
+											  )
+								      ).slice(0,4); // return top rated grandmasters
+              this.randomGoc = gocs[Math.floor(Math.random() * gocs.length)]; //get random goc
+          }
+      );
   }
+
+
+  // display a placeholder image if the picture of the selected grandamster is not available
+  noImage(goc: Goc): void {
+    if(goc.image !== this.placeHolder) {
+      goc.image = this.placeHolder;
+    }
+  }
+
 }
+
+
+
+
+
+//gocs.slice(1, 5)
